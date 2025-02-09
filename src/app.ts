416eb920359574a8be68fc87bd5f5369db2a2431
Benjamin-mne <bernjamin.delgado.elias@gmail.com>
2025-02-09 02:19:08 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express'
 import { SignalModel } from './models/signal-model'
 import { createSignalsRoutes } from './routes/signal-routes'
 import { config } from 'dotenv'
+import cors from 'cors'
+
 
 export function createApp (signalModel: SignalModel) {
   const PORT = process.env.PORT ?? 3000
   const app = express()
   config()
 
+  app.use(cors())
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
 
